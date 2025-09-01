b9cbd46662a8d9af215d8733b7324e5e6b6f5e46
LAVANESH-spec <lavaneshmahendran475@gmail.com>
2025-09-01 12:14:02 -0400

    while loop add and break statements modified

--- Dice_Rolling_Game_1.py
+++ Dice_Rolling_Game_1.py
@@ -1,13 +1,14 @@
 import random;
 
-#loop
-dice=input("Roll the dice? Enter Y or N:");
-if dice.lower() =="y":
- dice_1=random.randint(1,6);
- dice_2=random.randint(1,6);
- print(f"You rolled: {dice_1} and {dice_2}")
-elif(dice.lower()=="n"):
- print("Thank you!");
- break;
-else:
-    print("Bye!");
+while True:
+ dice=input("Roll the dice? Enter Y or N:");
+ if dice.lower() =="y":
+  dice_1=random.randint(1,6);
+  dice_2=random.randint(1,6);
+  print(f"You rolled: {dice_1} and {dice_2}")
+ elif(dice.lower()=="n"):
+  print("Thank you!");
+  break;
+ else:
+  print("Bye!");
+  break;
