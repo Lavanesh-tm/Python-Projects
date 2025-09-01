a017f216857d59eafdc9fc31f411d37e239560c1
LAVANESH-spec <lavaneshmahendran475@gmail.com>
2025-09-01 12:09:53 -0400

    dice rolled modified

--- Dice_Rolling_Game_1.py
+++ Dice_Rolling_Game_1.py
@@ -5,9 +5,9 @@ dice=input("Roll the dice? Enter Y or N:");
 if dice.lower() =="y":
  dice_1=random.randint(1,6);
  dice_2=random.randint(1,6);
- print(f"{dice_1}{dice_2}");
+ print(f"You rolled: {dice_1} and {dice_2}")
 elif(dice.lower()=="n"):
-  print("Thank you!");
-  break;
+ print("Thank you!");
+ break;
 else:
     print("Bye!");
