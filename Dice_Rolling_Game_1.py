f5eea05fe4e083954a93163f56fdc0fd53e78595
LAVANESH-spec <lavaneshmahendran475@gmail.com>
2025-09-01 12:05:13 -0400

    Dice_Rolling_Game_1.py Added

---
+++ Dice_Rolling_Game_1.py
@@ -0,0 +1,13 @@
+import random;
+
+#loop
+dice=input("Roll the dice? Enter Y or N:");
+if dice.lower() =="y":
+ dice_1=random.randint(1,6);
+ dice_2=random.randint(1,6);
+ print(f"{dice_1}{dice_2}");
+elif(dice.lower()=="n"):
+  print("Thank you!");
+  break;
+else:
+    print("Bye!");
